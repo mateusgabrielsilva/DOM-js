c8a2888a8abf586837a885c5da148a5348c87134
mateusgabrielsilva <mateus92.mgs@gmail.com>
2020-03-16 21:17:34 -0300

    Aprendendo DOM js

---
+++ todos.js
@@ -0,0 +1,42 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var btnElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer cafe',
+    'Estudar Javascript',
+    'Acessar comunidade da rocketseat'
+];
+
+function renderTodos() {
+
+    listElement.innerHTML = '';
+
+    for(todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+btnElement.onclick = addTodo;
